9070b6470d808a7c4af20e3433092fb0b1cb157c
zoni7 <53258847+zoni7@users.noreply.github.com>
2021-09-25 10:39:47 +0200

    tsr pract0

--- Year3/TSR/pract0/prova.js
+++ Year3/TSR/pract0/prova.js
@@ -1,4 +1,60 @@
-let person = {}
-person.name = "Peter"
-person.age=25
-console.log(person.district)
+/*
+function a() {
+    let a1=1
+    b(a1)
+}
+
+function b(p1) {
+    let b1=2
+    console.log(p1)
+    console.log(b1)
+    console.log(gl1)
+}
+a()
+var gl1=0
+*/
+/*
+function a() {
+    let a1=1
+    b(a1)
+    function b(p1) {
+        let b1=2
+        console.log(p1)
+        console.log(b1)
+        console.log(gl1)
+    }
+}
+
+
+a()
+let gl1=0
+*/
+
+// FUNCTIONS TO PRACTISE
+function x(a, b , ... args) {
+    for (let i in args) {
+        console.log("%dth arg: %s", i , args[i])
+    }
+    //console.log(arguments[1])
+}
+
+function f (a,b,c = "hola", d = 4) {
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+}
+
+x = 1
+
+function v() {
+    var x = 2
+    console.log(x)
+    console.log(global.x)
+}
+
+
+// OUTPUTS
+//x(1,2,4)
+//f(2)
+v()
